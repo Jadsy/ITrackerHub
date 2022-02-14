b65b5258f5e427efc637ff7b257debcee79856e9
Karim Jabri <jabrikarim02@gmail.com>
2022-02-15 00:13:34 +0200

    Added Severity board to dashboard

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container>
+  <!-- <v-container>
     <v-row>
       <v-col cols="12">
         <calender></calender>
@@ -13,23 +13,26 @@
         <recent-activity></recent-activity>
       </v-col>
     </v-row>
-  </v-container>
+  </v-container> -->
+  <severity-vue></severity-vue>
 </template>
 
 <script>
-import AddIssue from '../pages/MyIssuesPage/AddIssue.vue'
-import Calender from './Calender.vue'
-import ToDo from './ToDo.vue'
-import RecentActivity from "./RecentActivity.vue"
+// import AddIssue from '../pages/MyIssuesPage/AddIssue.vue'
+// import Calender from './Calender.vue'
+// import ToDo from './ToDo.vue'
+// import RecentActivity from "./RecentActivity.vue"
 //import PieChart from './PieChart.vue'
+import SeverityVue from './Severity.vue'
 
 export default {
   components: {
-    AddIssue,
-    Calender,
-    ToDo,
-    RecentActivity,
+    // AddIssue,
+    // Calender,
+    // ToDo,
+    // RecentActivity,
     //PieChart
+    SeverityVue
   },
 }
 </script>
---
+++ src/views/dashboard/Severity.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="3">
+                <v-card>
+                    <v-card-title>Minor</v-card-title>
+                </v-card>
+                <v-card>
+                    <v-card-title style="font-size:medium">Change the font zize to 15px</v-card-title>
+                </v-card>
+                <v-card>
+                    <v-card-title style="font-size:medium">Change the backround color to green</v-card-title>
+                </v-card>
+            </v-col>
+            <v-col cols="3">
+                <v-card>
+                    <v-card-title>Major</v-card-title>
+                </v-card>
+                <v-card>
+                    <v-card-title style="font-size:medium">Fix visual bug top left of home screen</v-card-title>
+                </v-card>
+                <v-card>
+                    <v-card-title style="font-size:medium">Kiss Bdeir on the forehead</v-card-title>
+                </v-card>
+            </v-col>
+            <v-col cols="3">
+                <v-card>
+                    <v-card-title >Critical</v-card-title>
+                </v-card>
+                <v-card>
+                    <v-card-title style="font-size:medium">Fix API connection</v-card-title>
+                </v-card>
+            </v-col>
+            <v-col cols="3">
+                <v-card>
+                    <v-card-title>Blocking</v-card-title>
+                </v-card>
+                <v-card>
+                    <v-card-title style="font-size:medium">Fuck Bdeir in the ass</v-card-title>
+                </v-card>
+                <v-card>
+                    <v-card-title style="font-size:medium">Test database security against ddos</v-card-title>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
