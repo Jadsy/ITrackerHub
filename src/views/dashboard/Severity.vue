fedeb11c4b87a6f7916b8f3408dad149594f7eee
Karim Jabri <jabrikarim02@gmail.com>
2022-02-15 16:16:28 +0200

    Fixed the severity table

--- src/views/dashboard/Severity.vue
+++ src/views/dashboard/Severity.vue
@@ -1,47 +1,43 @@
 <template>
     <v-container>
         <v-row>
-            <v-col cols="3">
+            <v-col cols="3" v-for="i in 4" :key="i">
                 <v-card>
-                    <v-card-title>Minor</v-card-title>
-                </v-card>
-                <v-card>
-                    <v-card-title style="font-size:medium">Change the font zize to 15px</v-card-title>
-                </v-card>
-                <v-card>
-                    <v-card-title style="font-size:medium">Change the backround color to green</v-card-title>
-                </v-card>
-            </v-col>
-            <v-col cols="3">
-                <v-card>
-                    <v-card-title>Major</v-card-title>
-                </v-card>
-                <v-card>
-                    <v-card-title style="font-size:medium">Fix visual bug top left of home screen</v-card-title>
-                </v-card>
-                <v-card>
-                    <v-card-title style="font-size:medium">Kiss Bdeir on the forehead</v-card-title>
-                </v-card>
-            </v-col>
-            <v-col cols="3">
-                <v-card>
-                    <v-card-title >Critical</v-card-title>
-                </v-card>
-                <v-card>
-                    <v-card-title style="font-size:medium">Fix API connection</v-card-title>
-                </v-card>
-            </v-col>
-            <v-col cols="3">
-                <v-card>
-                    <v-card-title>Blocking</v-card-title>
-                </v-card>
-                <v-card>
-                    <v-card-title style="font-size:medium">Fuck Bdeir in the ass</v-card-title>
-                </v-card>
-                <v-card>
-                    <v-card-title style="font-size:medium">Test database security against ddos</v-card-title>
+                    <v-card-title>{{severity[i-1].title}}</v-card-title>
+                    <v-divider horizontal></v-divider>
+                    <v-card v-for="issue in issues" :key="issue">
+                        <v-card-text v-if="issue.issueSeverityId===severity[i-1].id" align-left>
+                            <v-dialog v-model="dialog">
+                                <template v-slot:activator="{on, attrs}">
+                                    <v-btn block text v-bind="attrs" v-on="on">{{issue.title}}</v-btn>
+                                </template>
+                                <v-card>
+                                    <v-card-title>{{issue.title}}</v-card-title>
+                                    <v-divider horizontal></v-divider>
+                                    <v-card-text>
+                                        <p> Description: {{ issue.description }} </p>
+                                        <p> Creation Date: {{ issue.created }} </p>
+                                        <p> Time estimate: {{ issue.time_estimate }}</p>
+                                        <p> Issue Status: {{ severity[i - 1].title }} </p>
+                                    </v-card-text>
+                                </v-card>
+                            </v-dialog>
+                        </v-card-text>
+                    </v-card>
                 </v-card>
             </v-col>
         </v-row>
     </v-container>
 </template>
+
+<script>
+import issues from './issues.json'
+import severity from './severity.json'
+
+export default{
+    data:()=>({
+        issues:issues,
+        severity:severity,
+    }),
+}
+</script>
